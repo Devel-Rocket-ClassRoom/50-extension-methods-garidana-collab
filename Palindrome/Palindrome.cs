65a2b3e61b0e1b6d484590f0eb1b9d0553b11235
garidana-collab <garidana@gmail.com>
2026-03-15 21:42:47 +0900

    50 ALL CLEAR

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+public static class Palindrom
+{
+    public static bool IsPalindrome(this string str)
+    {
+        string letterOrNum = "";
+        if (str == null || str == string.Empty)
+        {
+            return false;
+            
+        }
+        else
+        {
+            foreach (var c in str)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    letterOrNum += c;
+                    
+                }
+            }
+            letterOrNum = letterOrNum.ToLower();
+
+            for (int i = 0, j = letterOrNum.Length - 1; i < j; i++, j--)
+            {
+                if (!(letterOrNum[i] == letterOrNum[j]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+    }
+}
--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,4 +1,26 @@
 using System;
+using System.Runtime.CompilerServices;
+using static System.Console;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+string tmt = "토마토";
+string grg = "기러기";
+string lvl = "level";
+string Lvl = "Level";
+string panama = "A man, a plan, a canal : Panama";
+string rcr = "race a car";
+string hello = "hello";
+string gnd = "가나다";
+string empty = "";
+
+WriteLine("=== 회문 판별 테스트 ===");
+WriteLine($"\"{tmt}\" -> {tmt.IsPalindrome()}");
+WriteLine($"\"{grg}\" -> {grg.IsPalindrome()}");
+WriteLine($"\"{lvl}\" -> {lvl.IsPalindrome()}");
+WriteLine($"\"{Lvl}\" -> {Lvl.IsPalindrome()}");
+WriteLine($"\"{panama}\" -> {panama.IsPalindrome()}");
+WriteLine($"\"{rcr}\" -> {rcr.IsPalindrome()}");
+WriteLine($"\"{hello}\" -> {hello.IsPalindrome()}");
+WriteLine($"\"{gnd}\" -> {gnd.IsPalindrome()}");
+WriteLine($"\"{empty}\" -> {empty.IsPalindrome()}");
+
